b1b17f94b9fb8a99a29593fc1a18d630c8673d77
Hugh Stephens <hughstephens@hughstephens.com>
2017-11-20 09:47:58 +1100

    Add delay for keypress

--- src/io/appium/android/ime/UnicodeIME.java
+++ src/io/appium/android/ime/UnicodeIME.java
@@ -84,7 +84,7 @@ public class UnicodeIME extends InputMethodService {
         int c = getUnicodeChar(keyCode, event);
 
         try{
-            Thread.sleep(50);
+            Thread.sleep(30);
         } catch(InterruptedException ex) {
             //do nothing
         }
